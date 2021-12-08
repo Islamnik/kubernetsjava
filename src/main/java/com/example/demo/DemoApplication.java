6fa81d843ea5454559155ab0d27ed5e52e61d076
IslamA <saadabuhamza@yahoo.com>
2021-12-08 22:03:44 +0300

    numbersplus

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(DemoApplication.class, args);
+        int i = 0;
+        while (i <= 10){
+            System.out.println(i);
+            i++;
+        }
     }
 
 }
